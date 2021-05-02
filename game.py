9c7b6f95f7dd96cd3da9ff4d47ec0df51c483435
Dor1-ux <83539251+Dor1-ux@users.noreply.github.com>
2021-05-02 15:27:09 +0300

    Add files via upload
    в заготовку добавился теннисный мяч

--- game.py
+++ game.py
@@ -5,7 +5,17 @@ win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
 window.fill(back)
-
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_w, player_h, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (player_w, player_h))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit( self.image, (self.rect.x, self.rect.y))
+tennis = GameSprite("mach_tennis.png", 300, 250, 70, 70, 0)
 clock = time.Clock()
 FPS = 60
 game = True
@@ -14,6 +24,7 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = FALSE
+    tennis.reset()
           
     display.update()
     clock.tick(FPS)
